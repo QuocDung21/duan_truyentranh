823d340e7a731e8d76cb3bbbd8d20f26b485c767
QuocDung21 <rongvang2357hn@gmail.com>
2023-08-30 23:27:55 +0700

    add

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -129,7 +129,7 @@ class IndexController extends Controller
         $chapter_moi = Chapter::with('truyen')
             ->orderBy('id', 'DESC')
             ->where('truyen_id', $truyenId->id)
-            ->take(3)
+            ->take(6)
             ->get();
         $chapter_dau = Chapter::with('truyen')
             ->orderBy('id', 'ASC')
