9373592e1d8bb038be7b9c18c39855f6b2d4f176
QuocDung21 <rongvang2357hn@gmail.com>
2023-08-31 12:39:02 +0700

    add

--- resources/views/pages/truyen.blade.php
+++ resources/views/pages/truyen.blade.php
@@ -91,7 +91,7 @@
         .truncate-text {
             overflow: hidden;
             display: -webkit-box;
-            -webkit-line-clamp: 6;
+            -webkit-line-clamp: 5;
             -webkit-box-orient: vertical;
             margin-bottom: 20px;
         }
