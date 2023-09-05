99c35a9c548552962f5795e043dbc2a83c371ed7
QuocDung21 <rongvang2357hn@gmail.com>
2023-09-06 02:23:48 +0700

    fix img

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,9 +7,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-{{--    <title>{{ Truện hot nhât }}</title>--}}
+    <title>{{ $info_webs->name }}</title>
 
-    <title>Truện hot nhất</title>
+    <meta name="ROBOTS" content="INDEX,FOLLOW" />
+
     <script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
 
     <link rel="icon" type="image/x-icon" href="{{ asset('public/uploads/info/logo/' . $info_webs->logo) }}">
